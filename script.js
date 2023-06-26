626b2f421021b5cfabaf2a98cfb9de6f6ce13e7f
erina saito <69622934+erina93416@users.noreply.github.com>
2023-06-26 17:47:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,81 @@
-$(".back-to-top").click(function(){
-    $("body,html").animate({
-        scrollTop:0
-    },"swing");
-    return false;
-});
+$(function(){
+    
+    //モーダルをクリックで開く
+    $('.modal-image-01').click(function(){
+        $('#modal01').fadeIn();
+    })
+    $('.modal-image-02').click(function(){
+        $('#modal02').fadeIn();
+    })
+    $('.modal-image-03').click(function(){
+        $('#modal03').fadeIn();
+    })
+
+    //モーダルを✖️ボタンで閉じる
+    $('.close-modal').click(function(){
+        $('.modal-content').fadeOut();
+    })
+
+    //スクロールアニメーション
+    ScrollReveal().reveal('.animation', { 
+        duration: 1600, 
+        origin: 'left', 
+        distance: '50px',
+        reset: true   
+    });
+
+    ScrollReveal().reveal('.animation2', { 
+        duration: 1600, 
+        origin: 'left', 
+        distance: '50px',
+        delay: 300,
+        reset: true   
+    });
+
+    ScrollReveal().reveal('.animation3', { 
+        duration: 1600, 
+        origin: 'left', 
+        distance: '50px',
+        delay: 600,
+        reset: true   
+    });
+
+    ScrollReveal().reveal('.animation4', { 
+        duration: 1600, 
+        origin: 'left', 
+        distance: '50px',
+        delay: 900,
+        reset: true   
+    });
+
+    ScrollReveal().reveal('.animation_t1', { 
+        duration: 1600, 
+        origin: 'top', 
+        distance: '50px',
+        reset: true   
+    });
+
+    ScrollReveal().reveal('.animation_t2', { 
+        duration: 1600, 
+        origin: 'top', 
+        distance: '50px',
+        delay: 300,
+        reset: true   
+    });
+
+    ScrollReveal().reveal('.animation_t3', { 
+        duration: 1600, 
+        origin: 'top', 
+        distance: '50px',
+        delay: 600,
+        reset: true   
+    });
+
+    ScrollReveal().reveal('.animation_t4', { 
+        duration: 1600, 
+        origin: 'top', 
+        distance: '50px',
+        delay: 900,
+        reset: true   
+    });
+})
